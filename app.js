3de0b53c79b6fa92ec4f1c21369cae957ecd284f
Cactus <cactusduper1@gmail.com>
2023-04-08 21:25:37 -0500

    Updated warning, removed more dlcs

--- app.js
+++ app.js
@@ -17,10 +17,40 @@ function filterSportsGames(gamesList) {
     return gamesList.filter(game => !sportsRegex.test(game));
 }
 
+function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + "; " + expires + "; path=/";
+}
+
+function getCookie(name) {
+    const cookieName = name + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookieArray = decodedCookie.split(';');
+    for (let i = 0; i < cookieArray.length; i++) {
+        let cookie = cookieArray[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(cookieName) === 0) {
+            return cookie.substring(cookieName.length, cookie.length);
+        }
+    }
+    return "";
+}
+
 async function selectGames() {
     const nsfwWarning = "This random game selector may display some NSFW games. By clicking OK, you confirm that you are 18+ and understand that filtering the games is annoying, and there may be some games left in the list still.";
-    if (!confirm(nsfwWarning)) {
-        return;
+    
+    // Check for the cookie
+    if (getCookie("nsfwWarningAccepted") !== "true") {
+        if (!confirm(nsfwWarning)) {
+            return;
+        } else {
+            // Store the cookie to remember that the user has accepted the warning
+            setCookie("nsfwWarningAccepted", "true", 365);
+        }
     }
     
     const numGames = parseInt(document.getElementById("num-games").value);
