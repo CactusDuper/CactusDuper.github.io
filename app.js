8e602ba27aa42d45e66bc555a78a64263e2f4664
Cactus <cactusduper1@gmail.com>
2023-04-08 20:53:07 -0500

    Quick update (css) and remove sports because yes

--- app.js
+++ app.js
@@ -12,16 +12,26 @@ function selectRandomGames(gamesList, numGames) {
     return Array.from({ length: numGames }, () => gamesList.splice(Math.floor(Math.random() * gamesList.length), 1)[0]);
 }
 
+function filterSportsGames(gamesList) {
+    const sportsRegex = /soccer|football|basketball|baseball|tennis|golf/i;
+    return gamesList.filter(game => !sportsRegex.test(game));
+}
+
 async function selectGames() {
     const numGames = parseInt(document.getElementById("num-games").value);
-    const gamesList = await loadGames();
+    let gamesList = await loadGames();
+    
+    // Filter out sports games
+    gamesList = filterSportsGames(gamesList);
+    
     const randomGames = selectRandomGames(gamesList, numGames);
 
     const gameListElement = document.getElementById("game-list");
     gameListElement.innerHTML = "";
 
     randomGames.forEach(game => {
-        const listItem = document.createElement("li");
+        const listItem = document.createElement("div");
+        listItem.classList.add("game-item");
         listItem.textContent = game;
         gameListElement.appendChild(listItem);
     });
